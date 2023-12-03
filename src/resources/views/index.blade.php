7164f9d5b6123a69fb7feaf5c1eb10e302f1b708
Philip Sørensen <git@ppsa.dk>
2023-12-03 13:04:23 +0100

    renamed package

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,7 +6,7 @@
 <x-crudimages::status />
 
 <a class="btn btn-light mb-3" href="{{ route($baseroute . 'create') }}">
-	<x-iconcomponents::plus-circle color="green" size="20" />
+	<x-bootstrapicons::plus-circle color="green" size="20" />
 	{{ trans('crudimages::image.upload') }}
 </a>
 
@@ -22,22 +22,22 @@
 			<td>{{ ucfirst($image->title) }}</td>
 			<td class="text-center">
 				<a href="{{ asset('storage/' . $image->location) }}" target="_blank" title="{{ trans('crudimages::image.show') }}">
-					<x-iconcomponents::search />
+					<x-bootstrapicons::search />
 				</a>
 			</td>
 			<td class="text-center">
 				<a class="" onclick="navigator.clipboard.writeText('{{ asset('storage/' . $image->location) }}')" style="cursor:pointer" title="{{ trans('crudimages::image.clipholder') }}">
-					<x-iconcomponents::card-image color="black" />
+					<x-bootstrapicons::card-image color="black" />
 				</a>
 			</td>
 			<td class="text-center">
 				<a href="{{ route($baseroute . 'edit', ['id' => $image->id]) }}" title="{{ trans('crudimages::image.edit') }}">
-					<x-iconcomponents::pencil-square />
+					<x-bootstrapicons::pencil-square />
 				</a>
 			</td>
 			<td class="text-center">
 				<a href="{{ route($baseroute . 'delete', ['id' => $image->id]) }}" onclick="return confirm('{{ trans('crudimages::image.delete.confirm') }}')">
-					<x-iconcomponents::trash color="red" />
+					<x-bootstrapicons::trash color="red" />
 				</a>
 			</td>
 		</tr>
